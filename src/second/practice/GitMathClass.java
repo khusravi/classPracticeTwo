9adcd01a4dbb0084f0647c31a7e8a39740b6a9e5
Khusravi <muhammad.khusravi@tekschool.us>
2022-06-22 13:35:14 -0700

    Math class added

---
+++ src/second/practice/GitMathClass.java
@@ -0,0 +1,15 @@
+package second.practice;
+
+public class GitMathClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 5;
+		int y = 4;
+		
+		int z = 5 + 4;
+		System.out.println(z);
+	}
+
+}
