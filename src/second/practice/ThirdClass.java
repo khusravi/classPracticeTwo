5ed49921838dbdb027bb13d8998f5d7f243b741a
Khusravi <muhammad.khusravi@tekschool.us>
2022-06-22 13:44:55 -0700

    new class added

---
+++ src/second/practice/ThirdClass.java
@@ -0,0 +1,11 @@
+package second.practice;
+
+public class ThirdClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Second exercise of Git Hub repo");
+	}
+
+}
