92cbedc1689b43f06877f21b0f359a75e6be783c
Khusravi <muhammad.khusravi@tekschool.us>
2022-06-22 13:48:56 -0700

    the fourth class added to project

---
+++ src/second/practice/TheFourthOne.java
@@ -0,0 +1,12 @@
+package second.practice;
+
+public class TheFourthOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Practice makes you perfect");
+
+	}
+
+}
